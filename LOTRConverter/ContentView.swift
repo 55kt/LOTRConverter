a68746773a1e77953812aacaf293b1c95d8dd2be
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-21 11:25:10 +0300

    Content View Structure

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -2,13 +2,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background Image
+            Color(.gray)
+                .ignoresSafeArea()
+            
+            VStack {
+                // Prancing Pony Image
+                
+                // Currency Exchange Text
+                
+                // Currency Conversion Section
+                HStack {
+                    // Left Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency image
+                            
+                            // Currency Text
+                        }
+                        
+                        // TextField
+                    }
+                    
+                    // Equal sign
+                    
+                    // Right Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency image
+                            
+                            // Currency Text
+                        }
+                        
+                        // TextField
+                    }
+                }
+                
+                // Info Button
+            }
         }
-        .padding()
     }
 }
 
