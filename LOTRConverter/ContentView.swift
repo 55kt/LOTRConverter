70145834d73f856e4a97297d35732dcf02a43351
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-21 12:08:10 +0300

    Adding Views To Design

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -4,13 +4,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background Image
-            Color(.gray)
+            Image(.background)
+                .resizable()
                 .ignoresSafeArea()
             
             VStack {
                 // Prancing Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Currency Conversion Section
                 HStack {
@@ -19,29 +27,56 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // TextField
+                        Text("TextField")
+                            .foregroundStyle(.white)
                     }
                     
                     // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right Conversion Section
                     VStack {
                         // Currency
                         HStack {
-                            // Currency image
-                            
                             // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // TextField
+                        Text("TextField")
+                            .foregroundStyle(.white)
                     }
                 }
                 
+                Spacer()
+                
                 // Info Button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
         }
     }
