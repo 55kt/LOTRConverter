91058d3eda0033a1c839e6205b0f6c139757d842
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-22 11:56:25 +0300

    Computed Properties

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -1,9 +1,39 @@
-
+import SwiftUI
 
 enum Currency: Double {
-    case goldpiece = 1
-    case goldpenny = 0.14
-    case silverpiece = 0.42
-    case silverpenny = 0.11
-    case copperpenny = 0.39
+    case goldPiece = 1
+    case goldPenny = 0.14
+    case silverPiece = 0.42
+    case silverPenny = 0.11
+    case copperPenny = 0.39
+    
+    var image: ImageResource {
+        switch self {
+        case .goldPiece:
+            return .goldpiece
+        case .goldPenny:
+            return .goldpenny
+        case .silverPiece:
+            return .silverpiece
+        case .silverPenny:
+            return .silverpenny
+        case .copperPenny:
+            return .copperpenny
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .goldPiece:
+            return "Gold Piece"
+        case .goldPenny:
+            return "Gold Penny"
+        case .silverPiece:
+            return "Silver Piece"
+        case .silverPenny:
+            return "Silver Penny"
+        case .copperPenny:
+            return "Copper Penny"
+        }
+    }
 }
